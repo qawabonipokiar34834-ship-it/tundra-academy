ca765deb73c130353a733b92f4bb3db9d4e7c59e
qawabonipokiar34834-ship-it <qawabonipokiar34834@gmail.com>
2025-11-17 19:43:57 +0300

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // <-- Эта строка гарантирует, что App.jsx будет прочитан
   ],
   theme: {
     extend: {},
